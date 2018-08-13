5e2b801d51dd191fd07bfa7a28f65f12c7634ad7
DESKTOP-VBV357E\carlo <carlos_em7@hotmail.com>
2018-08-13 00:44:21 -0500

    set static route3

--- bancompara/settings.py
+++ bancompara/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'bancompara.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'bancompara',
-		'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		#'ENGINE': 'django.db.backends.mysql',
+		#'NAME': 'bancompara',
+		#'USER': 'root',
+        #'PASSWORD': 'root',
+        #'HOST': 'localhost',
+        #'PORT': '',
     }
 }
 
