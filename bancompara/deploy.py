630e165734d5747ccbf2c84f73cfb9579695d20d
DESKTOP-VBV357E\carlo <carlos_em7@hotmail.com>
2018-08-13 18:09:36 -0500

    finally

--- bancompara/deploy.py
+++ bancompara/deploy.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from whitenaise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
@@ -17,3 +17,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bancompara.deploy_settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
